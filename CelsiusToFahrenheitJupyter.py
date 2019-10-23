b3c6162b57d5e2dd98950c192c1a72cdd4150a84
ArtuR <44754904+ArtuRUA01@users.noreply.github.com>
2019-10-23 21:31:48 +0300

    Update CelsiusToFahrenheitJupyter.py

--- CelsiusToFahrenheitJupyter.py
+++ CelsiusToFahrenheitJupyter.py
@@ -15,7 +15,7 @@ celsius_q    = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
 fahrenheit_a = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 
 for i,c in enumerate(celsius_q):
-  print("{} градусів Цельсія = {} градусів Фаренгейта".format(c, fahrenheit_a[i]))
+  print("{} градуси Цельсія = {} градуси Фаренгейта".format(c, fahrenheit_a[i]))
 
 
 # In[13]:
@@ -24,7 +24,7 @@ for i,c in enumerate(celsius_q):
 model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(loss='mean_squared_error',optimizer=tf.keras.optimizers.Adam(0.1))
 history = model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=False)
-print("Завершили тренировку моделі")
+print("Завершили тренування моделі")
 
 
 # In[14]:
